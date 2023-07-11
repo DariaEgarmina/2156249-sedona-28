b5f45d93cd60173c5ef349569e220c85a1fe0c41
Daria Egarmina <d.egarmina@gmail.com>
2023-07-11 13:27:25 +0300

    Добаратывает сборку

--- gulpfile.js
+++ gulpfile.js
@@ -38,19 +38,19 @@ const html = () => {
 const scripts = () => {
   return gulp.src('source/js/*.js')
     .pipe(gulp.dest('build/js'))
-    .pipe(browser.stream()); // Что это такое?? так и не поняла, было в предложеном коде от htmlacademy
+    .pipe(browser.stream());
 }
 
 // Images
 
 const optimizeImages = () => {
-  return gulp.src('source/img/**/*.{png,jpg}')
+  return gulp.src('source/img/**/*.{png,jpg,svg}')
     .pipe(squoosh())
     .pipe(gulp.dest('build/img'))
 }
 
 const copyImages = () => {
-  return gulp.src('source/img/**/*.{png,jpg}')
+  return gulp.src('source/img/**/*.{png,jpg,svg}')
     .pipe(gulp.dest('build/img'))
 }
 
@@ -69,9 +69,9 @@ const createWebp = () => {
 const makeStack = () => {
   return gulp.src('source/img/svg/*.svg')
     .pipe(svgo())
-    .pipe(stacksvg({ output: `sprite` }))
+    .pipe(stacksvg({ output: 'sprite' }))
     .pipe(rename('stack.svg'))
-    .pipe(gulp.dest('build/img/svg')); //Почему-то не работает :(((
+    .pipe(gulp.dest('build/img/stack'));
 }
 
 // Copy
@@ -99,7 +99,7 @@ const clean = () => {
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
